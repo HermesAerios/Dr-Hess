da036ff0a1bf4383bcbbbd50a0f4db9aac6c6363
HermesAerios <72360989+HermesAerios@users.noreply.github.com>
2026-07-04 00:30:13 +0200

    Update Patient.ts

---
+++ src/domain/models/Patient.ts
@@ -0,0 +1,24 @@
+export interface Patient {
+    id: string;
+    name: string;
+    age: number;
+    sex: 'M' | 'F';
+    profession: string;
+    weight: number;
+    atcd: string[];
+    symptoms: string[];
+    pathologyId: string;
+    timeInSystem: number;
+    vitals: {
+        heartRate: number;
+        systolicBP: number;
+        diastolicBP: number;
+        oxygenSaturation: number;
+        potassium: number;
+        creatinine: number;
+    };
+    state: {
+        consciousnessScore: number;
+        creatinineClearance: number;
+    };
+}
